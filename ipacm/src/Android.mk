3ebc37a6508cc8444c2f2cc65d0c3882824bd6fb
Chih-Hung Hsieh <chh@google.com>
2018-10-26 22:50:25 -0700

    Allow implicit-fallthrough warnings in IPACM_Main.cpp.
    It will be a global error by default.
    
    Test: make checkbuild
    Bug: 112564944
    Change-Id: Iae843ff17e0fcd5b84514d76dd6840106446305f
    Exempt-From-Owner-Approval: do not block on new warnings

--- ipacm/src/Android.mk
+++ ipacm/src/Android.mk
@@ -38,6 +38,9 @@ LOCAL_CFLAGS += $(shell if [ -a $(filetoadd) ] ; then echo -include $(filetoadd)
 filetoadd = bionic/libc/kernel/arch-arm/asm/byteorder.h
 LOCAL_CFLAGS += $(shell if [ -a $(filetoadd) ] ; then echo -include $(filetoadd) ; fi ;)
 
+# Allow warnings in IPACM_Main.cpp until they are fixed.
+LOCAL_CFLAGS += -Wno-error=implicit-fallthrough
+
 LOCAL_SRC_FILES := IPACM_Main.cpp \
 		IPACM_EvtDispatcher.cpp \
 		IPACM_Config.cpp \
